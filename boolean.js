ae708c9e345c5fb3a2e6288564ecb754de2e8310
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-01-30 18:24:20 +0900

    1/30 boolean-datatype

---
+++ boolean.js
@@ -0,0 +1,12 @@
+// boolean = true or false , 0 or 1 
+// 동적 ui 스위치 버튼 등 쓰임
+
+const amiFat = true;
+console.log(amiFat);
+
+// 자바스크립트 데이터 타입 = null(값이 없음)
+// 자바스크립트 데이터 타입 = undefined = 1. 변수는 있지만 값이 정의되지 않음)
+// 2. 메모리 안에는 존재하지만(공간) 값이 들어가지 않음
+
+let something;
+console.log(something);
