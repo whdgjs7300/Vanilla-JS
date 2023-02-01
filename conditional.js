295034e77b705e02e3891e48262152c0d077d364
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-02-01 19:36:47 +0900

    2/1 conditional recap

--- conditional.js
+++ conditional.js
@@ -12,7 +12,13 @@ console.log(isNaN(age2))
 
 // 조건이 boolean 값이나 boolean 값으로 나타낼 수 있을 때 사용
 if(isNaN(age2)) {
-    console.log("숫자를 입력하세요");
+    console.log("나이를 입력하세요");
+} else if(age2 < 18){
+    console.log("18세 미만입니다");
+} else if(age => 18 && age <= 50){
+    console.log("술을 마실 수 있습니다");
 } else {
-    console.log("숫자가 맞습니다");
+    console.log("술을 마실 수 없습니다");
 }
+
+
