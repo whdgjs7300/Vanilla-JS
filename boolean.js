b0a3f743a604ca2ff1d7d0bf8f062812d305ed59
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-05-10 20:33:57 +0900

    Update boolean.js

--- boolean.js
+++ boolean.js
@@ -6,7 +6,7 @@ console.log(amiFat);
 
 // 자바스크립트 데이터 타입 = null(값이 없음)
 // 자바스크립트 데이터 타입 = undefined = 1. 변수는 있지만 값이 정의되지 않음)
-// 2. 메모리 안에는 존재하지만(공간) 값이 들어가지 않음
+// 2. 메모리 안에는 존재하지만(공간) 값이 들어가지 않음 ㅇㅇ
 
 let something;
 console.log(something);
